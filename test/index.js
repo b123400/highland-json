111db49d9561cf1443675047bfca1f213a968076
b123400 <i@b123400.net>
2020-01-16 14:20:49 +0900

    Update test case

--- test/index.js
+++ test/index.js
@@ -158,3 +158,77 @@ export const testKeyValuePair = test => {
 		test.done();
 	});
 };
+
+export const testArrayWithCustomStringify = test => {
+	test.expect(1);
+
+	const input = ['a', 'b', 'c'];
+
+	const expected = ['a1', 'b1', 'c1'];
+
+	const customStringify = (val, stream)=> JSON.stringify(val + '1');
+
+	_(input)
+	.through(stringify({stringify: customStringify}))
+	.toArray(results => {
+		let result = results.join('');
+		result = JSON.parse(result);
+		test.deepEqual(result, expected);
+		test.done();
+	});
+};
+
+export const testNestedArrayWithCustomStringify = test => {
+	test.expect(1);
+
+	const input = [
+		_(['a']),
+		_(['b']),
+		_(['c']),
+	];
+
+	const expected = [['a1'], ['b1'], ['c1']];
+
+	const customStringify = (val, stream)=> JSON.stringify(val + '1');
+
+	_(input)
+	.through(stringify({stringify: customStringify}))
+	.toArray(results => {
+		let result = results.join('');
+		result = JSON.parse(result);
+		test.deepEqual(result, expected);
+		test.done();
+	});
+};
+
+export const testObjectWithCustomStringify = test => {
+	test.expect(1);
+
+	const input = {
+		nums: _([1, 2, 3]),
+		strings: _(['a', 'b', 'c']),
+	};
+
+	const expected = {
+		nums: [10, 20, 30],
+		strings: ['a1', 'b1', 'c1'],
+	};
+
+	const customStringify = (val, stream)=> {
+		if (stream === input.nums) {
+			return JSON.stringify(val * 10);
+		}
+		if (stream === input.strings) {
+			return JSON.stringify(val + '1');
+		}
+		throw new Error("Unreachable");
+	};
+
+	_([input])
+	.through(stringifyObj({stringify: customStringify}))
+	.toArray(results => {
+		const result = JSON.parse(results.join(''));
+		test.deepEqual(result, expected);
+		test.done()
+	});
+};
